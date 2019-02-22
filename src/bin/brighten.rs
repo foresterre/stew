94bcab8940d3f72cffb71a9edf4d2cfb32cdbeee
Martijn Gribnau <garm@ilumeo.com>
2019-02-22 19:56:17 +0100

    Work on brighten.

--- src/bin/brighten.rs
+++ src/bin/brighten.rs
@@ -4,28 +4,33 @@ use stew_lib::operations::operation_by_name;
 use stew_lib::operations::OpArg;
 use stew_lib::run;
 
-const COMMAND_NAME: &str = "blur";
-const FIRST_ARG: &str = "σ";
+const COMMAND_NAME: &str = "brighten";
+const FIRST_ARG: &str = "VALUE";
 
 fn main() -> Result<(), String> {
     let app = get_app_skeleton(COMMAND_NAME).arg(
         Arg::with_name(FIRST_ARG)
             .help(
-                "σ represents the amount to blur by. It should be a 32 bit floating point number.",
+                "VALUE represents the amount to brighten by. This value can both be positive \
+                (increase brightness) or negative (decrease brightness). \
+                The VALUE should be a 32 bit integer.",
             )
             .takes_value(true)
+            .allow_hyphen_values(true) // TODO{}: This doesn't seem to work
             .required_unless_one(&["license", "dep_licenses"])
-            .index(1)
+            .index(1),
     );
+
+    panic!("Work in progress!");
 
     let matches = app.get_matches();
 
     if let Some(input1) = matches.value_of(FIRST_ARG) {
         let parsed = input1
-            .parse::<f32>()
-            .map_err(|_| "The first argument of the blur command should be a floating point.")?;
+            .parse::<i32>()
+            .map_err(|_| "The argument of the brighten command should be an integer.")?;
 
-        let op = operation_by_name(COMMAND_NAME, OpArg::FloatingPoint(parsed));
+        let op = operation_by_name(COMMAND_NAME, OpArg::Integer(parsed));
 
         run(&matches, Some(op?))
     } else {
