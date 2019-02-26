de03404080af2885ee87fd510cccb21a2c8b48a0
Martijn Gribnau <garm@ilumeo.com>
2019-02-26 14:30:03 +0100

    Add additional display tests using pipes and piped output.

--- play.sh
+++ play.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm target/out.png
 cargo build
-cat resources/botanical.jpg | ./target/debug/brighten -o target/out.png -10 && eog target/out.png
+# cat resources/botanical.jpg | ./target/debug/crop -o target/out.png 10 10 210 210
+cat resources/botanical.jpg | ./target/debug/crop -f bmp 10 20 180 210 | ./target/debug/blur 15 |  ./target/debug/brighten -o target/out.png 70
+# cat resources/botanical.jpg | ./target/debug/blur 80 |  ./target/debug/brighten -o target/out.png -70
+# cat resources/botanical.jpg | ./target/debug/blur 10 > target/out.png
+eog target/out.png
 
