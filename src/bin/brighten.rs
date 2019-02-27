6d86271d30c1da2079a0e33a6d3249557fd7404b
Martijn Gribnau <garm@ilumeo.com>
2019-02-27 18:02:16 +0100

    Put 'brighten' operation in same style as others.

--- src/bin/brighten.rs
+++ src/bin/brighten.rs
@@ -7,12 +7,12 @@ use stew_lib::run;
 use stew_lib::run_display_licenses;
 
 const COMMAND_NAME: &str = "brighten";
-const FIRST_ARG: &str = "VALUE";
+const ARG1: &str = "VALUE";
 
 fn main() -> Result<(), String> {
     let app = get_app_skeleton(COMMAND_NAME)
         .arg(
-            Arg::with_name(FIRST_ARG)
+            Arg::with_name(ARG1)
                 .help(
                     "VALUE represents the amount to brighten by. This value can both be positive \
                      (increase brightness) or negative (decrease brightness). \
@@ -27,20 +27,24 @@ fn main() -> Result<(), String> {
         .global_setting(AppSettings::AllowLeadingHyphen);
 
     let matches = app.get_matches();
-    let license = matches.is_present("license");
-    let dep_licenses = matches.is_present("dep_licenses");
+    let license_display = matches.is_present("license") || matches.is_present("dep_licenses");
 
-    if let Some(input1) = matches.value_of(FIRST_ARG) {
-        let parsed = input1
-            .parse::<i32>()
-            .map_err(|_| "The argument of the brighten command should be an integer.")?;
-
-        let op = operation_by_name(COMMAND_NAME, OpArg::Integer(parsed));
-
-        run(&matches, Some(op?))
-    } else if license || dep_licenses {
+    if license_display {
         run_display_licenses(&matches)
     } else {
-        Err(format!("{} definition was unexpected.", COMMAND_NAME))
+        match matches.value_of(ARG1) {
+            Some(v) => {
+                let op = operation_by_name(COMMAND_NAME, OpArg::Integer(parse_i32(v)?));
+
+                run(&matches, Some(op?))
+            }
+            _ => Err("brighten requires exactly 1 argument (32 bit integer).".to_string()),
+        }
     }
+}
+
+fn parse_i32(input: &str) -> Result<i32, String> {
+    input
+        .parse::<i32>()
+        .map_err(|_| "The argument of the brighten command should be an integer (i32).".to_string())
 }
